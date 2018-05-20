e946d49d9739eeeda2754375edac08c4df61797d
yexiuzhou <10855912+yexiuzhou@users.noreply.github.com>
2018-05-19 19:01:19 -0700

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+public class TwoSum
+{
+    public int[] twoSum(int[] nums, int target) {
+        int num1;
+        int num2;
+        int sum;
+        int[] r = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = 0; j < nums.length; j++){
+                num1 = nums[i];
+                num2 = nums[j];
+                sum = num1 + num2;
+                if (i!=j && sum==target){
+                    r[0] = i;
+                    r[1] = j;
+                    return r;
+                }
+            }
+        }
+        return r;
+    }
+}
